46d924fc1278d5e2b5c076218add37cd8fa5a346
1 <1@DESKTOP-HMUOOM8>
2024-04-09 10:12:17 -0300

    Adicionar arquivos de projeto.

---
+++ retanguloex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace retanguloex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("coloque a base do retângulo:");
+            double basedoretangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Coloque a altura do retângulo:");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double area = basedoretangulo * altura;
+
+            Console.WriteLine($"A área do retângulo é: {area}");
+        }
+    }
+}
